735f24e1f52ab6fca40aad8d5c8f347752090fd3
GitHub Action <action@github.com>
2024-12-24 12:18:55 +0000

    doc

---
+++ crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["hirofa_quickjs_sys"];
+//{"start":21,"fragment_lengths":[20]}
---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["hirofa_quickjs_sys",["",[],["lib.rs","static-functions.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[63]}
